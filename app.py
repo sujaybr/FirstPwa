623b9e9bbabf337ce18a9a508695709415b9fc15
sujaybr <sujaybr9@gmail.com>
2018-01-26 13:03:19 +0530

    four

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def error():
     return app.send_static_file("error.html")
 
+@app.route('/sw.js')
+def sw():
+    return app.send_static_file('sw.js')
+
 if __name__ == "__main__":
     app.run(debug = True)
